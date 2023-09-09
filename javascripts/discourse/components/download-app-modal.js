3b8b5f6604ee595e1df4450c3a4c8635bdee492e
ThienNguyenInfina <thien.nguyen@realstake.io>
2023-09-09 15:41:54 +0700

    test

--- javascripts/discourse/components/download-app-modal.js
+++ javascripts/discourse/components/download-app-modal.js
@@ -1,6 +1,12 @@
 import Component from "@glimmer/component";
 import { inject as service } from "@ember/service";
+import { action } from "@ember/object";
 
 export default class DownloadAppModal extends Component {
   @service routes;
+
+  @action
+  openDownloadLink() {
+    window.open("https://onelink.to/seohomepageinfina", "_blank");
+  }
 }
