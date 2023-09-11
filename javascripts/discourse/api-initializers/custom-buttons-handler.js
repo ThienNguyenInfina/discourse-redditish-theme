b76c8af20bf12ca10d758da45476e50f153f6222
ThienNguyenInfina <thien.nguyen@realstake.io>
2023-09-11 15:22:22 +0700

    test

---
+++ javascripts/discourse/api-initializers/custom-buttons-handler.js
@@ -0,0 +1,52 @@
+import { withPluginApi } from "discourse/lib/plugin-api";
+import DownloadAppModal from "../components/modal/download-app-modal";
+
+export default {
+  name: "tesing",
+
+  initialize(owner) {
+    withPluginApi("0.11.4", (api) => {
+      api.onPageChange(() => {
+        const user = api.getCurrentUser();
+
+        async function showModalDownload() {
+          owner.lookup("service:modal").show(DownloadAppModal);
+        }
+
+        // If user is not logged in
+        if (!user) {
+          const replyButton = document.querySelector(
+            "#topic-footer-buttons .btn-icon-text"
+          );
+          const likeButton = document.querySelector(
+            ".actions .double-button .toggle-like"
+          );
+
+          if (replyButton) {
+            replyButton.addEventListener(
+              "click",
+              function (event) {
+                event.preventDefault();
+                event.stopPropagation();
+                showModalDownload();
+              },
+              true
+            );
+          }
+
+          if (likeButton) {
+            likeButton.addEventListener(
+              "click",
+              function (event) {
+                event.preventDefault();
+                event.stopPropagation();
+                showModalDownload();
+              },
+              true
+            );
+          }
+        }
+      });
+    });
+  },
+};
