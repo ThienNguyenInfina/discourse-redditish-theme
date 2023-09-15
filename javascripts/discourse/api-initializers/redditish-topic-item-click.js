915f3371fe32f73b343eaf64e851c4cdaeb44cf4
ThienNguyenInfina <thien.nguyen@realstake.io>
2023-09-15 15:03:48 +0700

    test

--- javascripts/discourse/api-initializers/redditish-topic-item-click.js
+++ javascripts/discourse/api-initializers/redditish-topic-item-click.js
@@ -7,43 +7,38 @@ export default {
 
   initialize(owner) {
     withPluginApi("0.8", (api) => {
-      api.modifyClass("component:topic-list-item", {
-        pluginId: "redditish-theme",
-
-        click(event) {
-          let target = event.target;
-          if (!target) {
-            return this._super(event);
-          }
-
-          if (
-            (target.nodeName === "A" && !target.closest(".raw-link")) ||
-            target.closest(".badge-wrapper")
-          ) {
-            return !!wantsNewWindow(event);
-          }
-
-          if (target.classList.contains("custom-topic-layout")) {
-            if (wantsNewWindow(event)) {
-              window.open(this.topic.lastUnreadUrl, "_blank");
-              return false;
-            }
-            return this.navigateToTopic(this.topic, this.topic.lastUnreadUrl);
-          }
-
-          if (target.closest(".share-toggle")) {
-            owner.lookup("service:modal").show(ShareTopicModal, {
-              model: {
-                category: this.topic.category,
-                topic: this.topic,
-              },
-            });
-            return true;
-          }
-
-          return this.navigateToTopic(this.topic, this.topic.lastUnreadUrl);
-        },
-      });
+      // api.modifyClass("component:topic-list-item", {
+      //   pluginId: "redditish-theme",
+      //   click(event) {
+      //     let target = event.target;
+      //     if (!target) {
+      //       return this._super(event);
+      //     }
+      //     if (
+      //       (target.nodeName === "A" && !target.closest(".raw-link")) ||
+      //       target.closest(".badge-wrapper")
+      //     ) {
+      //       return !!wantsNewWindow(event);
+      //     }
+      //     if (target.classList.contains("custom-topic-layout")) {
+      //       if (wantsNewWindow(event)) {
+      //         window.open(this.topic.lastUnreadUrl, "_blank");
+      //         return false;
+      //       }
+      //       return this.navigateToTopic(this.topic, this.topic.lastUnreadUrl);
+      //     }
+      //     if (target.closest(".share-toggle")) {
+      //       owner.lookup("service:modal").show(ShareTopicModal, {
+      //         model: {
+      //           category: this.topic.category,
+      //           topic: this.topic,
+      //         },
+      //       });
+      //       return true;
+      //     }
+      //     return this.navigateToTopic(this.topic, this.topic.lastUnreadUrl);
+      //   },
+      // });
     });
   },
 };
