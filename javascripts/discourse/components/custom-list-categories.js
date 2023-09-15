ec22a635f12ffedcafb22c363df654764ada91eb
ThienNguyenInfina <thien.nguyen@realstake.io>
2023-09-15 14:29:24 +0700

    test

--- javascripts/discourse/components/custom-list-categories.js
+++ javascripts/discourse/components/custom-list-categories.js
@@ -1,18 +1,17 @@
-import Component from "@glimmer/component";
-import { inject as service } from "@ember/service";
 import { action } from "@ember/object";
+import { inject as service } from "@ember/service";
+import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
 
 export default class CustomListCategories extends Component {
   @service store;
+  @service route;
   @tracked categories = null;
 
   @action
   async getCategories() {
     let categoryList;
-
     categoryList = await this.store.findFiltered("category");
-
-    this.categories = categoryList.category_list.categories ?? [];
+    this.categories = categoryList?.site?.categories ?? [];
   }
 }
