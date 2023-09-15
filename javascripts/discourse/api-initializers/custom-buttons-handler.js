506a807040ea64233eefb55e7829db655d649511
ThienNguyenInfina <thien.nguyen@realstake.io>
2023-09-15 15:06:45 +0700

    test

--- javascripts/discourse/api-initializers/custom-buttons-handler.js
+++ javascripts/discourse/api-initializers/custom-buttons-handler.js
@@ -7,41 +7,45 @@ export default {
   initialize(owner) {
     withPluginApi("0.8", (api) => {
       api.onPageChange(() => {
-        // const user = api.getCurrentUser();
-        // async function showModalDownload() {
-        //   owner.lookup("service:modal").show(DownloadAppModal);
-        // }
-        // // If user is not logged in
-        // if (!user) {
-        //   const replyButton = document.querySelector(
-        //     "#topic-footer-buttons .btn-icon-text"
-        //   );
-        //   const likeButton = document.querySelector(
-        //     ".actions .double-button .toggle-like"
-        //   );
-        //   if (replyButton) {
-        //     replyButton.addEventListener(
-        //       "click",
-        //       function (event) {
-        //         event.preventDefault();
-        //         event.stopPropagation();
-        //         showModalDownload();
-        //       },
-        //       true
-        //     );
-        //   }
-        //   if (likeButton) {
-        //     likeButton.addEventListener(
-        //       "click",
-        //       function (event) {
-        //         event.preventDefault();
-        //         event.stopPropagation();
-        //         showModalDownload();
-        //       },
-        //       true
-        //     );
-        //   }
-        // }
+        const user = api.getCurrentUser();
+
+        async function showModalDownload() {
+          owner.lookup("service:modal").show(DownloadAppModal);
+        }
+
+        // If user is not logged in
+        if (!user) {
+          const replyButton = document.querySelector(
+            "#topic-footer-buttons .btn-icon-text"
+          );
+          const likeButton = document.querySelector(
+            ".actions .double-button .toggle-like"
+          );
+
+          if (replyButton) {
+            replyButton.addEventListener(
+              "click",
+              function (event) {
+                event.preventDefault();
+                event.stopPropagation();
+                showModalDownload();
+              },
+              true
+            );
+          }
+
+          if (likeButton) {
+            likeButton.addEventListener(
+              "click",
+              function (event) {
+                event.preventDefault();
+                event.stopPropagation();
+                showModalDownload();
+              },
+              true
+            );
+          }
+        }
       });
     });
   },
