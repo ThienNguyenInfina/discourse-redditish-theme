449f58b5e3a52d248b0507080031f3061555a279
ThienNguyenInfina <thien.nguyen@realstake.io>
2023-11-01 15:31:04 +0700

    change let

--- javascripts/discourse/api-initializers/custom-initial-events.js
+++ javascripts/discourse/api-initializers/custom-initial-events.js
@@ -11,7 +11,7 @@ export default {
           // Select the element with class "cooked"
           const cookedElement = document.querySelector(".cooked");
           const img = document.querySelector(".cooked img"); // This will select the first <img> element on the page
-          const categoryName = "";
+          let categoryName = "";
           const element = document.querySelector("span.category-name");
           if (element) {
             categoryName = element.textContent;
